996cc59bf26da270f038c315b4715c249ac7cf74
Pierre Libran <pedro89@hotmail.fr>
2025-08-31 21:55:50 +0200

    improving 1.1

--- app/controllers/wireguard_clients_controller.rb
+++ app/controllers/wireguard_clients_controller.rb
@@ -6,13 +6,34 @@ class WireguardClientsController < ApplicationController
     @wireguard_client = current_user.wireguard_clients.new
   end
 
+  # def create
+  #   # Use the service to create the WireGuard client and subscription
+  #   creator = WireguardClientCreator.new(current_user, wireguard_client_params[:name])
+  #   @wireguard_client = creator.call
+
+  #   if @wireguard_client.persisted?
+  #     redirect_to user_wireguard_client_path(current_user, @wireguard_client), notice: "WireGuard client created successfully."
+  #   else
+  #     render :new
+  #   end
+  # end
+
   def create
+    # Generate a unique 6-character alphanumeric token
+    client_name = loop do
+      random_name = SecureRandom.alphanumeric(6).upcase
+      break random_name unless WireguardClient.exists?(name: random_name)
+    end
+
+    Rails.logger.info "Creating WireGuard client: #{client_name}"
+
     # Use the service to create the WireGuard client and subscription
-    creator = WireguardClientCreator.new(current_user, wireguard_client_params[:name])
+    creator = WireguardClientCreator.new(current_user, client_name)
     @wireguard_client = creator.call
 
     if @wireguard_client.persisted?
-      redirect_to user_wireguard_client_path(current_user, @wireguard_client), notice: "WireGuard client created successfully."
+      redirect_to user_wireguard_client_path(current_user, @wireguard_client),
+                  notice: "WireGuard client created successfully."
     else
       render :new
     end
@@ -29,7 +50,11 @@ class WireguardClientsController < ApplicationController
 
   private
 
+  # def wireguard_client_params
+  #   params.require(:wireguard_client).permit(:name)
+  # end
+
   def wireguard_client_params
-    params.require(:wireguard_client).permit(:name)
+    params.require(:wireguard_client).permit(:other_attributes) # exclude :name
   end
 end
--- app/models/wireguard_client.rb
+++ app/models/wireguard_client.rb
@@ -3,6 +3,8 @@ class WireguardClient < ApplicationRecord
   belongs_to :user
   has_one :subscription, dependent: :destroy
 
+  validates :name, uniqueness: true
+  
   def expired?
     expires_at.present? && expires_at < Time.current
   end
