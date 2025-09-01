430d7ff0938dbdd9b53f7647cadfd9196934af6a
Pierre Libran <pedro89@hotmail.fr>
2025-09-01 22:25:12 +0200

    remasterized

--- app/models/wireguard_client.rb
+++ app/models/wireguard_client.rb
@@ -1,10 +1,9 @@
 class WireguardClient < ApplicationRecord
   # Association with User
-  belongs_to :user
-  has_one :subscription, dependent: :destroy
+  belongs_to :subscription
 
   validates :name, uniqueness: true
-  
+
   def expired?
     expires_at.present? && expires_at < Time.current
   end
