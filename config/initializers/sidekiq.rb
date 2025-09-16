ad53964ef9868e41f730084eec58a58736e31221
Pierre Libran <pedro89@hotmail.fr>
2025-09-16 23:27:15 +0200

    Switch to sidekiq-cron and fix scheduling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 # config/initializers/sidekiq.rb
 require 'sidekiq'
-require 'sidekiq/scheduler'
+require 'sidekiq-cron'
 
 redis_url = ENV['REDIS_URL']
 
@@ -10,22 +10,14 @@ Sidekiq.configure_server do |config|
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 
-  # Explicit scheduler setup
   config.on(:startup) do
-    schedule_file = Rails.root.join('config', 'sidekiq.yml')
-
-    if File.exist?(schedule_file)
-      loaded_schedule = YAML.load_file(schedule_file)
-      if loaded_schedule.key?(:scheduled)
-        Sidekiq.schedule = loaded_schedule[:scheduled]
-        Sidekiq::Scheduler.reload_schedule!
-        Rails.logger.info "Sidekiq-Scheduler: Loaded schedule: #{Sidekiq.schedule.inspect}"
-      else
-        Rails.logger.error "Sidekiq-Scheduler: No :scheduled key found in YAML"
-      end
-    else
-      Rails.logger.error "Sidekiq-Scheduler: YAML file not found at #{schedule_file}"
-    end
+    Sidekiq::Cron::Job.load_from_hash(
+      'revoke_expired_subscriptions' => {
+        'cron'  => '0 */6 * * *',  # Every 6 hours
+        'class' => 'RevokeExpiredSubscriptionsJob',
+        'queue' => 'default'
+      }
+    )
   end
 end
 
