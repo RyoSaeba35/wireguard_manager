a171206a74d225d74ea8cc9324f24f650af69cbc
Pierre Libran <pedro89@hotmail.fr>
2025-09-16 23:12:52 +0200

    Force-enable Sidekiq-Scheduler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
+# config/initializers/sidekiq.rb
+require 'sidekiq'
+require 'sidekiq-scheduler'  # Force-load the scheduler
+
 redis_url = ENV['REDIS_URL']
 
 Sidekiq.configure_server do |config|
@@ -5,6 +9,17 @@ Sidekiq.configure_server do |config|
     url: redis_url,
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
+
+  # Explicit scheduler setup
+  config.on(:startup) do
+    if defined?(Sidekiq::Scheduler)
+      Sidekiq.schedule = YAML.load_file(Rails.root.join('config', 'sidekiq.yml'))
+      Sidekiq::Scheduler.reload_schedule!
+      Rails.logger.info("Sidekiq-Scheduler: Loaded schedule from config/sidekiq.yml")
+    else
+      Rails.logger.error("Sidekiq-Scheduler: Gem not loaded! Check bundle.")
+    end
+  end
 end
 
 Sidekiq.configure_client do |config|
@@ -13,3 +28,4 @@ Sidekiq.configure_client do |config|
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
 end
+
