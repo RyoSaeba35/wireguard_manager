56c25df3cf82c359d5c7fe4e95b671e057452b79
Pierre Libran <pedro89@hotmail.fr>
2025-12-06 17:00:36 +0100

    updating dynos management 1.0

--- config/puma.rb
+++ config/puma.rb
@@ -20,7 +20,11 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+
+# Number of worker processes (set to 1 for Heroku Hobby/Basic dynos)
+workers ENV.fetch("WEB_CONCURRENCY", 1)
+
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
@@ -32,3 +36,11 @@ plugin :tmp_restart
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# Preload the application before forking worker processes
+preload_app!
+
+# Reconnect to the database in each worker process
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
