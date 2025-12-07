24377fb50e744340af5cd5c85475aa155cffe028
Pierre Libran <pedro89@hotmail.fr>
2025-12-07 15:36:42 +0100

    improving mailer 2.2

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -10,6 +10,7 @@ class DownloadsController < ApplicationController
     client_name = filename.gsub('.png', '')
     Rails.logger.info "Filename: #{filename}, Client Name: #{client_name}"
     client = current_user.wireguard_clients.find_by(name: client_name)
+
     if client && client.qr_code.attached?
       redirect_to rails_blob_path(client.qr_code, disposition: "inline")
     else
@@ -23,26 +24,13 @@ class DownloadsController < ApplicationController
     unless filename.end_with?('.conf')
       filename += '.conf'
     end
-    client_name = filename.gsub('.conf', '').gsub('Vulcain_', '')
+    client_name = filename.gsub('.conf', '')
     Rails.logger.info "Filename: #{filename}, Client Name: #{client_name}"
-
+    Rails.logger.info "Current User Wireguard Clients: #{current_user.wireguard_clients.pluck(:name)}"
     client = current_user.wireguard_clients.find_by(name: client_name)
+
     if client && client.config_file.attached?
-      file_data = client.config_file.download
-
-      if file_data.blank?
-        Rails.logger.error "Config file data is empty for client: #{client_name}"
-        redirect_to root_path, alert: "Config file is empty."
-        return
-      end
-
-      Rails.logger.info "File data size: #{file_data.bytesize}"
-
-      response.headers['Content-Type'] = 'text/x-config'
-      response.headers['Content-Disposition'] = "attachment; filename=\"#{filename}\""
-      response.headers['Content-Length'] = file_data.bytesize.to_s
-
-      send_data file_data, type: 'text/x-config', disposition: 'attachment'
+      redirect_to rails_blob_path(client.config_file, disposition: "attachment")
     else
       Rails.logger.error "Config file not found for client: #{client_name}"
       redirect_to root_path, alert: "Config file not found."
