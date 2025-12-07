bd42a64772179584ac177cb1f3ed645cd4f4c17c
Pierre Libran <pedro89@hotmail.fr>
2025-12-07 14:59:07 +0100

    improving mailer 1.4

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -24,17 +24,17 @@ class DownloadsController < ApplicationController
     unless filename.end_with?('.conf')
       filename += '.conf'
     end
-    # Remove "Vulcain_" prefix before extracting client_name
+
     client_name = filename.gsub('.conf', '').gsub('Vulcain_', '')
     Rails.logger.info "Filename: #{filename}, Client Name: #{client_name}"
 
-    Rails.logger.info "Current User Wireguard Clients: #{current_user.wireguard_clients.pluck(:name)}"
     client = current_user.wireguard_clients.find_by(name: client_name)
-
     if client && client.config_file.attached?
-      # Force the downloaded filename to match the requested `filename`
-      response.headers['Content-Disposition'] = "attachment; filename=\"#{filename}\""
-      redirect_to rails_blob_path(client.config_file, disposition: "attachment")
+      file_data = client.config_file.download
+      send_data file_data,
+                filename: filename,
+                disposition: 'attachment',
+                type: 'text/x-config'
     else
       Rails.logger.error "Config file not found for client: #{client_name}"
       redirect_to root_path, alert: "Config file not found."
