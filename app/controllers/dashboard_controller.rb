961b8c4184a9e5f1c6a0d2dd2cb5f6e328cd7bbe
Pierre Libran <pedro89@hotmail.fr>
2025-12-07 09:42:51 +0100

    improving css 3.4

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,12 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @vpn_server_ips = Server.where(active: true).pluck(:wireguard_server_ip)
-    # Fetch the user's IP (ensure this is set correctly in your controller)
-    @user_ip = request.remote_ip
-    
+    @user_ip = request.env['HTTP_X_FORWARDED_FOR'] || request.remote_ip
+    @vpn_server_ips = Rails.cache.fetch("active_vpn_server_ips", expires_in: 12.hours) do
+      Server.where(active: true).pluck(:wireguard_server_ip)
+    end
+    @country = fetch_country(@user_ip)
+
     # Fetch the "active" subscription, but only if it's truly not expired
     @active_subscription = current_user.subscriptions.find_by(
       status: "active",
@@ -23,5 +25,16 @@ class DashboardController < ApplicationController
   def setup
     @has_subscription = current_user.subscriptions.active.any?
     @active_subscription = current_user.subscriptions.active.first
+  end
+
+  private
+
+  def fetch_country(ip)
+    Rails.cache.fetch("country_#{ip}", expires_in: 1.hour) do
+      response = Net::HTTP.get(URI("http://ip-api.com/json/#{ip}"))
+      JSON.parse(response)['country']
+    rescue StandardError
+      nil
+    end
   end
 end
