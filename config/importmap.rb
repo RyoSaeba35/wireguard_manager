0a862e6f9b351f21ac746ec16e80854f88c7cccc
Pierre Libran <pedro89@hotmail.fr>
2026-04-18 10:18:17 +0200

    Fix: Use Bootstrap Bundle to include Popper.js

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@splidejs/splide", to: "@splidejs--splide.js" # @4.1.4
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.2/dist/js/bootstrap.esm.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
