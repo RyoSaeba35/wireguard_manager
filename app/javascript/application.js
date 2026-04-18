f8e450a7f2354df7a950d6b6ebfc8e1b6e1b0011
Pierre Libran <pedro89@hotmail.fr>
2026-04-18 10:15:20 +0200

    Fix: Remove CSS imports from JavaScript (already loaded via CDN)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "bootstrap"
-import "bootstrap/dist/css/bootstrap";
-// app/javascript/packs/application.js
-import "@splidejs/splide/dist/css/splide.min.css";
+
+// Import Splide JavaScript only (CSS is already in layout)
 import Splide from "@splidejs/splide";
 window.Splide = Splide; // Make Splide globally available
-
