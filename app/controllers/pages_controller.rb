0c24ad6328abc4ad6f631c3deaa701ea7ffe2761
Pierre Libran <pedro89@hotmail.fr>
2026-04-20 14:38:27 +0200

    Fixing subscription history

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:subscriptions_expired]
+  skip_before_action :authenticate_user!, only: [:privacy, :terms, :logging]
 
   def privacy
   end
