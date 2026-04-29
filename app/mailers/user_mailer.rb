e507b980c0eff204989fb35c24ae8627c2295d72
Pierre Libran <pedro89@hotmail.fr>
2026-04-29 17:01:39 +0200

    changing user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,12 +7,12 @@ class UserMailer < ApplicationMailer
     @subscription = subscription
 
     # Attach the PDF
-    pdf_path = Rails.root.join('public', 'pdfs', 'Vulcain_VPN_Setup_Guide.pdf')
-    if File.exist?(pdf_path)
-      attachments['Vulcain_VPN_Setup_Guide.pdf'] = File.read(pdf_path)
-    else
-      Rails.logger.error "PDF not found at #{pdf_path}"
-    end
+    # pdf_path = Rails.root.join('public', 'pdfs', 'Vulcain_VPN_Setup_Guide.pdf')
+    # if File.exist?(pdf_path)
+    #   attachments['Vulcain_VPN_Setup_Guide.pdf'] = File.read(pdf_path)
+    # else
+    #   Rails.logger.error "PDF not found at #{pdf_path}"
+    # end
 
     # Attach the logo as an inline image
     logo_path = Rails.root.join('app/assets/images/Vulcain_VPN_logo_3.png')
