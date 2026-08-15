7a53cbba71e78423eb01a992a9f0d34ef7b3a8aa
Pierre Libran <pedro89@hotmail.fr>
2026-08-15 20:32:18 +0200

    Fixing : removing locales from sitemap

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -4,18 +4,18 @@ class SitemapsController < ApplicationController
 
   def index
     @pages = [
-      { loc: root_url,                   priority: "1.0", changefreq: "weekly"  },
-      { loc: blog_url,                   priority: "0.9", changefreq: "weekly"  },
-      { loc: blog_vpn_expats_url,        priority: "0.8", changefreq: "monthly" },
-      { loc: blog_vpn_logs_url,          priority: "0.8", changefreq: "monthly" },
-      { loc: blog_vpn_voyage_url,        priority: "0.8", changefreq: "monthly" },
-      { loc: blog_vpn_privee_url,        priority: "0.8", changefreq: "monthly" },
-      { loc: blog_vpn_vs_grands_url,     priority: "0.8", changefreq: "monthly" },
-      { loc: blog_vpn_teletravail_url,   priority: "0.8", changefreq: "monthly" },
-      { loc: blog_vpn_android_url,       priority: "0.8", changefreq: "monthly" },
-      { loc: logging_url,                priority: "0.6", changefreq: "monthly" },
-      { loc: privacy_url,                priority: "0.5", changefreq: "monthly" },
-      { loc: terms_url,                  priority: "0.5", changefreq: "monthly" },
+      { loc: root_url(locale: nil),                 priority: "1.0", changefreq: "weekly"  },
+      { loc: blog_url(locale: nil),                 priority: "0.9", changefreq: "weekly"  },
+      { loc: blog_vpn_expats_url(locale: nil),      priority: "0.8", changefreq: "monthly" },
+      { loc: blog_vpn_logs_url(locale: nil),        priority: "0.8", changefreq: "monthly" },
+      { loc: blog_vpn_voyage_url(locale: nil),      priority: "0.8", changefreq: "monthly" },
+      { loc: blog_vpn_privee_url(locale: nil),      priority: "0.8", changefreq: "monthly" },
+      { loc: blog_vpn_vs_grands_url(locale: nil),   priority: "0.8", changefreq: "monthly" },
+      { loc: blog_vpn_teletravail_url(locale: nil), priority: "0.8", changefreq: "monthly" },
+      { loc: blog_vpn_android_url(locale: nil),     priority: "0.8", changefreq: "monthly" },
+      { loc: logging_url(locale: nil),              priority: "0.6", changefreq: "monthly" },
+      { loc: privacy_url(locale: nil),              priority: "0.5", changefreq: "monthly" },
+      { loc: terms_url(locale: nil),                priority: "0.5", changefreq: "monthly" },
     ]
     respond_to do |format|
       format.xml
